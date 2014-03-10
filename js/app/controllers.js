a263d1cc1665088a4c79100f32efa661deb0a069
Paulo Scardine <paulos@xtend.com.br>
2014-03-10 06:20:54 -0300

    mapa

--- js/app/controllers.js
+++ js/app/controllers.js
@@ -4,18 +4,36 @@ angular.module('graficos.controllers', [])
         '$http',
         function ($scope, $http) {
             $scope.data = [];
+            $scope.title = "Indice Paulista de Responsabilidade Social (grupos)";
+            $scope.item = {};
+            $scope.ano = 2010;
+            $scope.codigo = 7;
+            $scope.anos = [2008, 2010];
 
-            $http({method: 'GET', url: 'data/variables.json'}).
-                success(function (data, status, headers, config) {
+
+            $http({method: 'GET', url: 'data/variaveis.json'})
+                .success(function (data, status, headers, config) {
                     $scope.data = data.menu;
-                }).
-                error(function (data, status, headers, config) {
+                    $scope.data[0].collapsed = $scope.data[0].itens[0].collapsed = false;
+                    $scope.item.selectNodeLabel($scope.data[0].itens[0].itens[0]);
+                })
+                .error(function (data, status, headers, config) {
                     $scope.data = {
                         nome: "ERRO",
                         id: "erro",
                         itens: ""
                     };
                 });
+
+            $scope.$watch(function() {return $scope.item.currentNode;}, function(curr, prev) {
+                if(typeof curr == 'undefined') return;
+                if(curr && isNaN(parseInt(curr.id))) return;
+                if(curr && curr.anos) {
+                    $scope.ano = parseInt(curr.anos.split(',').pop());
+                    $scope.anos = curr.anos.split(',');
+                }
+                if(curr && curr.id) $scope.codigo = curr.id;
+            });
         }
     ]);
 
--- js/app/directives.js
+++ js/app/directives.js
@@ -14,4 +14,111 @@ angular.module('graficos.directives', [])
                 };
             }
         }
-    });
+    })
+    .directive('vegaMap', function() {
+        return {
+            restrict: 'E',
+            templateUrl: '/graficos/js/app/html/vega-map.html',
+            transclude: true,
+            scope: {
+                url: '@',
+                info: '=',
+                width: '@',
+                height: '@',
+                feature: '@',
+                geometry: '@',
+                parse: '=',
+                title: '@',
+                current: '=',
+                options: '='
+            },
+            link: function(scope, element, attrs) {
+                scope.$watch('url', function(curr, prev) {
+                    if(curr) {
+                        d3.tsv(curr, function(data) {
+                            if(data === null) {
+                                alert("Falha ao carregar arquivo " + curr);
+                                return;
+                            }
+                            scope.data = {};
+                            $.each(data, function(i, elem) {
+                                scope.data[elem.ibge] = elem;
+                            });
+                            scope.$apply();
+                        });
+                    }
+                });
+
+                var spec = {
+                    "width": parseInt(scope.width),
+                    "height": parseInt(scope.height),
+                    "data": [
+                        {
+                            "name": "source",
+                            "url": scope.url,
+                            "format": {"type": "tsv", "parse": scope.parse}
+                        },
+                        {
+                            "name": "topology",
+                            "url": scope.geometry,
+                            "format": {"type": "topojson", "feature": scope.feature},
+                            "transform": [
+                                {
+                                    "type": "geopath",
+                                    "value": "data",
+                                    "projection": "mercator",
+                                    "center": [-48.0, -22.5459886162543],
+                                    "scale": 5000
+                                },
+                                {
+                                    "type": "zip",
+                                    "key": "data.properties.CD_GEOCODM",
+                                    "with": "source",
+                                    "withKey": "data.ibge",
+                                    "as": "value",
+                                    "default": null
+                                },
+                                {"type":"filter", "test":"d.path!=null && d.value!=null"}
+                            ]
+                        }
+                    ],
+                    "scales": [
+                        {
+                            "name": "color",
+                            "type": "quantize",
+                            "domain": [0, 5],
+                            "range": ["#f7fbff", "#deebf7", "#c6dbef", "#9ecae1", "#6baed6"]
+                        }
+                    ],
+                    "marks": [
+                        {
+                            "type": "path",
+                            "from": {"data": "topology"},
+                            "properties": {
+                                "enter": { "path": {"field": "path"} },
+                                "update": { "fill": {"scale":"color", "field":"value.data.valor"} },
+                                "hover": { "fill": {"value": "red"} }
+                            }
+                        }
+                    ]
+                };
+
+                function parse(spec) {
+                    vg.parse.spec(spec, function(chart) {
+                        var m = chart({el: scope.vis[0]});
+                        m.on("mouseover", function(event, item) {
+                            scope.info = scope.data[item.datum.data.properties.CD_GEOCODM];
+                            scope.$apply();
+                        });
+                        /* m.on("mouseout", function(event, item) {
+                            scope.info = undefined;
+                            scope.$apply();
+                        }); */
+                        m.update();
+                    });
+                }
+                parse(spec);
+            }
+        }
+    })
+;
